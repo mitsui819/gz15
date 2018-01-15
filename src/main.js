86bf74a53f461bd24b9035ddd3c2f9ecab7e6340
mitsui <517857026@qq.com>
2018-01-15 11:01:05 +0800

    1-15早

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
+import 'normalize.css';
+import './less/index.less';
+
+
 import Vue from 'vue';
 import Router from 'vue-router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk.index.css'
 
 Vue.use(VueRouter);
+Vue.use(ElementUI);
 
 import App from './component/App.vue';
 
 import routerConfig from './router/index.js';
 
 import axios from 'axios';
-axios.get('http:www.baidu.com').then(function(res){
-    console.log(res);
-});
+axios.defaults.baseURL = 'http://localhost:8899';
+Vue.prototype.$http = axios;
+import api from './js/api-config.js';
+Vue.prototype.$api = api;
 
 new Vue({
     el:'#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,27 @@
 import Login from'../component/login/Login.vue';
 import Admin from'../component/login/Admin.vue';
+import GoodsCgList from'../component/admin/goods/category/List.vue';
+import GoodsCgEdit from'../component/admin/goods/category/Edit.vue';
+import GoodsCtList from'../component/admin/goods/content/List.vue';
+import GoodsCtEdit from'../component/admin/goods/content/Edit.vue';
 // import Register from'../component/register/Register.vue';
+
+let adminChildren=[
+    {name:'goodsCtList',path:'goods/content/list',component:GoodsCtList},
+    {name:'goodsCtEdit',path:'goods/content/edit/:id',component:GoodsCtEdit},
+    {name:'goodsCtAdd',path:'goods/content/add',component:GoodsCtEdit},
+
+
+    {name:'goodsCgList',path:'goods/category/list',compontent:GoodsCgList},
+    {name:'goodsCgEdit',path:'goods/category/edit/:id',compontent:GoodsCgEdit},
+    {name:'goodsCgAdd',path:'goods/category/add',compontent:GoodsCgEdit}
+];
+
 export default{
 
     routes:[
         {name:'login',path:'/login',component:Login},
-        {name:'admin',path:'/admin',component:Admin}
+        {path:'admin',redirect:'/admin'},
+        {name:'admin',path:'/admin',component:Admin,children:adminChildren}
     ]
 };
